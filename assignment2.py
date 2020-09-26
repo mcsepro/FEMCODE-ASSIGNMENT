c2b873c64604b1b2e90bd32b981793e78d65280c
mcsepro <mcsepro@users.noreply.github.com>
2020-09-26 10:25:08 +0100

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,15 @@
+#average of four numbers
+a=40
+b=20
+c=10
+d=4
+sum_of_numbers=a+b+c+d
+number_score=4
+average=sum_of_numbers/number_score
+print(average)
+
+#to compute 10% of a number
+number=200
+percentage=10/100*number
+print(percentage)
+
